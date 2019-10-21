24f7a18a0c7c5de461fe368207dc94bf750e11cb
Leonardo Gil <leogilc@gmail.com>
2019-10-20 21:35:54 -0300

    mapa

---
+++ navbar.js
@@ -0,0 +1,30 @@
+document.write('<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="menu">' +
+    '<div class="container">' +
+    '<a class="navbar-brand" href="home.html">' +
+    '<img src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">' +
+    ' Plante Fácil</a>' +
+    '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">' +
+    '<span class="navbar-toggler-icon"></span>' +
+    '</button>' +
+    '<div class="collapse navbar-collapse" id="navbarResponsive">' +
+    '<ul class="navbar-nav ml-auto">' +
+    '<li class="nav-item">' +
+    '<a class="nav-link" href="#">About</a>' +
+    '</li>' +
+    '<li class="nav-item">' +
+    '<a class="nav-link" href="#">Services</a>' +
+    '</li>' +
+    '<li class="nav-item">' +
+    '<a class="nav-link" href="#">Contact</a>' +
+    '</li>' +
+    '<li class="nav-item">' +
+    '<a class="nav-link" href="mapa.html">Mapa</a>' +
+    '</li>' +
+    '<li class="nav-item">' +
+    '<a class="nav-link" href="index.html">Sair</a>' +
+    '</li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</nav>'
+)
