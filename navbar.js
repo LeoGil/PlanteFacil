fe034285927f277193439c9e8be85d557a1cfb33
Leonardo Gil <leogilc@gmail.com>
2019-10-29 01:06:17 -0300

    correcao arquivo

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ document.write('<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-
     '<div class="container">' +
     '<a class="navbar-brand" href="home.html">' +
     '<img src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">' +
-    ' Plante FÃ¡cil</a>' +
+    ' Plante Fácil</a>' +
     '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">' +
     '<span class="navbar-toggler-icon"></span>' +
     '</button>' +
