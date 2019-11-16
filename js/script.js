432d0afab25d4e263dd8f1d6b49135b669a3495c
Leonardo Gil <leogilc@gmail.com>
2019-11-16 00:01:32 -0300

    parte administrativa

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ var comprar_arvore = function (valor) {
 
     $('#span_saldo').html(localStorage.saldo)
 }
+
+function loadPage(page) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', page, true);
+    xhr.onreadystatechange = function () {
+        document.getElementById('corpo_pill').innerHTML = this.responseText;
+        document.documentElement.scrollTop = 0;
+    };
+    xhr.send();
+}
